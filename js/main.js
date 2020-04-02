81d448cfdcf117a7acafaf46ca6e0005c608323b
JonathanDutt <jonathand.dutt@georgebrown.ca>
2020-04-02 16:39:36 -0400

    Infi load/sticky bar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+let theStateOfTheInterface = (event) => { 
+  
+	let winH = document.documentElement.clientHeight
+	let docH = document.documentElement.scrollHeight
+	let winY = window.scrollY
+	let maxY = docH - winH
+  
+	if (winY >= maxY) {
+	
+	  document.querySelector('#main').innerHTML += `
+	  <article class="game-wrapper">
+              <h2>Nioh 2</h2>
+              <iframe class="video-frame" src="https://www.youtube.com/embed/R7jxm8SGRtQ" frameborder="0"></iframe>
+              <p>The sequel to Team Ninja's excellent blend of the action and Souls-like genres is here, and it succeeds as a wonderful evolution of everything that the first game did right, even though it's many systems can sometimes slow the otherwise blistering pace. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt tempore suscipit dignissimos mollitia quia culpa dolorum autem, ad qui sit perspiciatis doloremque tenetur. Repellendus culpa cumque sequi voluptas, similique non.</p>
+              <h2>Black Mesa</h2>
+              <iframe class="video-frame" src="https://www.youtube.com/embed/KaJO14_TAbA" frameborder="0"></iframe>
+              <p>Black Mesa (formerly known as Black Mesa: Source) is a first-person shooter video game, developed and published by Crowbar Collective. It is a third-party remake of Half-Life in the Source game engine. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quis autem cumque in excepturi corporis, sint minus, adipisci dolor quod laboriosam veritatis qui optio enim, assumenda libero ut ullam sed.</p>
+              <h2>Black Mesa</h2>
+              <iframe class="video-frame" src="https://www.youtube.com/embed/y8hhBhjyrrg" frameborder="0"></iframe>
+              <p>LCS Highlights ALL GAMES Week 7 Day 2 Spring 2020 - 100 vs IMT, FLY vs TSM, GG vs DIG, CLG vs EG Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quis autem cumque in excepturi corporis, sint minus, adipisci dolor quod laboriosam veritatis qui optio enim, assumenda libero ut ullam sed.</p>
+              <h2>Black Mesa</h2>
+              <iframe class="video-frame" src="https://www.youtube.com/embed/WSLZfawK0Xk" frameborder="0"></iframe>
+              <p>James Duggan reviews Tom Clancy's The Division 2: Warlords of New York expansion on PC. Also available on Xbox One and PlayStation 4. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quis autem cumque in excepturi corporis, sint minus, adipisci dolor quod laboriosam veritatis qui optio enim, assumenda libero ut ullam sed.</p>
+            </article>
+		`
+	}
+}
+
 let $show = document.querySelector('.show')
 
 // A function that goes to the start of the show
@@ -63,8 +91,29 @@ let windowLoaded = () => {
 }
 
 
-
-
-
 // Kick off!
 window.addEventListener('load', windowLoaded)
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickyScroll()};
+
+// Get the navbar
+let $menu = document.getElementById("menu");
+
+// Get the offset position of the navbar
+let sticky = $menu.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyScroll() {
+  if (window.pageYOffset >= sticky) {
+    $menu.classList.add("sticky")
+  } else {
+    $menu.classList.remove("sticky");
+  }
+}
+
+
+
+
+window.addEventListener('scroll', theStateOfTheInterface)
+window.addEventListener('resize', theStateOfTheInterface)
